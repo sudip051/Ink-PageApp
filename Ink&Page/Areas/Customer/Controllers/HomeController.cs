ba5f334872d4dd9024c578ce612fc8b24e3938b7
sudipadhikari <sudipadk051@gmail.com>
2026-02-07 21:22:34 +0545

    feat: Add homepage and product details page

--- Ink&Page/Areas/Customer/Controllers/HomeController.cs
+++ Ink&Page/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 using Ink_Page.Models;
+using InkPage.DataAccess.Repository.IRepository;
+using InkPage.Models.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Ink_Page.Areas.Customer.Controllers
@@ -8,15 +10,24 @@ namespace Ink_Page.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
+        }
+
+        public IActionResult Details(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(u=>u.Id == productId, includeProperties: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
